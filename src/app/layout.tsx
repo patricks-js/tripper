43207c0c2451720a7ecc75d3559f2a561fe1f234
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-10-03 00:46:37 -0300

    chore: setup biome as linter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { cn } from "@/lib/cn";
 
 export const metadata: Metadata = {
   title: "Tripper",
-  description: "Organize viagens com facilidade, definindo destino, datas e convidando participantes.",
+  description:
+    "Organize viagens com facilidade, definindo destino, datas e convidando participantes.",
 };
 
 export default function RootLayout({
@@ -16,8 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("antialiased font-sans", GeistSans.variable, GeistMono.variable )}>
-        {children}  
+      <body
+        className={cn(
+          "antialiased font-sans",
+          GeistSans.variable,
+          GeistMono.variable,
+        )}
+      >
+        {children}
       </body>
     </html>
   );
