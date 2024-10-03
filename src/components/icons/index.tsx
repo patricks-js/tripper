e6dcd8ffe59cc62d277b818847ae057db95f2de8
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-10-03 09:56:09 -0300

    feat: update icons set

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,5 +1,19 @@
-import { MapPin } from "lucide-react";
+import {
+  ArrowRight,
+  AtSign,
+  Calendar,
+  MapPin,
+  Plus,
+  Settings2,
+  UserPlus,
+} from "lucide-react";
 
 export const Icons = {
   location: MapPin,
+  arrowRight: ArrowRight,
+  calendar: Calendar,
+  addUser: UserPlus,
+  settings: Settings2,
+  at: AtSign,
+  plus: Plus,
 };
