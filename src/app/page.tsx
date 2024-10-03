0f06c821112550f4b7f660aa64c0a096ca37d297
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-10-03 09:58:58 -0300

    feat: create terms and policies component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
+import { TermsAndPolicies } from "@/components/terms-and-policies";
+
 export default function Home() {
   return (
     <main className="grid place-items-center bg-shape bg-center bg-no-repeat min-h-screen">
-      <div className="space-y-10">
+      <div className="flex flex-col gap-10 items-center">
         <div className="flex flex-col items-center gap-2">
           <h1 className="text-3xl font-bold font-mono">Tripp.er</h1>
           <p className="text-sm max-w-md">
             Convide seus amigos e planeje sua próxima viagem!
           </p>
         </div>
-        <p className="text-center text-muted-foreground text-xs max-w-md">
-          Ao planejar sua viagem pela tripp.er você automaticamente concorda com
-          nossos <a href="/">termos de uso</a> e{" "}
-          <a href="/">políticas de privacidade</a>.
-        </p>
+        <TermsAndPolicies />
       </div>
     </main>
   );
